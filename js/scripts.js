ef14d56780c4b7738282a48acb77106a355abfa1
david ayala <dpayala@hotmail.com>
2016-08-12 10:17:44 -0700

    adds show/hide result for name

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
+$(function() {
+  $("button").click(function(event){
+    event.preventDefault();
+    var name = $("#name").val();
 
+
+    $(".name").text(name);
+
+
+
+    $("#result").show();
+
+  });
+});
