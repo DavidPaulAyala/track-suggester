9f63891c99c34cb1337ccc5047ff437348dd5bb5
david ayala <dpayala@hotmail.com>
2016-08-12 14:23:00 -0700

    first if statement works

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-$(function() {
-  $("button").click(function(event){
+$(document).ready(function(){
+
+
+
+  $("#track-suggest form").submit(function(event) {
     var name = $("#name").val();
     var website = parseInt($("select#style").val());
     var size = parseInt($("select#size").val());
@@ -8,6 +11,20 @@ $(function() {
     var freeTime = parseInt($("select#time").val());
 
     var track = "TEST";
+
+    if (website <= 0 && freeTime <= 0) {
+      {
+      track = "C#/.Net";
+      }
+    }
+
+
+
+
+
+
+
+
 
 
 
