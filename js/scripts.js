8ed09f50a0a88c75ed3564bddf739af89fd3d865
david ayala <dpayala@hotmail.com>
2016-08-12 13:18:33 -0700

    enables variables read by response

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
 $(function() {
   $("button").click(function(event){
-    event.preventDefault();
     var name = $("#name").val();
-    var website = parseInt($("#style").val());
-    var size = parseInt($("#size").val());
+    var website = parseInt($("select#style").val());
+    var size = parseInt($("select#size").val());
     var mobile = parseInt($("input:radio[name=mobile]:checked").val());
     var system = $("input:radio[name=os]:checked").val();
-    var freeTime = parseInt($("#time").val());
-    var track = "notenoughinfo";
+    var freeTime = parseInt($("select#time").val());
+
+    var track = "TEST";
+
+
 
     $(".name").text(name);
     $("#trackid").text(track);
     $("#result").show();
+    event.preventDefault();
 
   });
 });
