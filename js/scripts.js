3e3e919dff48b02ab34e41aa7711e58862a74467
david ayala <dpayala@hotmail.com>
2016-08-12 11:28:36 -0700

    labels variables and changes answer values

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,15 @@ $(function() {
   $("button").click(function(event){
     event.preventDefault();
     var name = $("#name").val();
-
+    var website = parseInt($("#style").val());
+    var size = parseInt($("#size").val());
+    var mobile = parseInt($("input:radio[name=mobile]:checked").val());
+    var system = $("input:radio[name=os]:checked").val();
+    var freeTime = parseInt($("#time").val());
+    var track = "notenoughinfo";
 
     $(".name").text(name);
-
-
-
+    $("#trackid").text(track);
     $("#result").show();
 
   });
