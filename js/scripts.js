25ff73b998b8dd3e0c9e8f9eb47510dd497b15a9
David Ayala <dpayala@hotmail.com>
2016-08-14 18:26:35 -0700

    adds multiple fuctionality to else if

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ $(function() {
   $("button").click(function(event){
     var name = $("#name").val();
     var website = parseInt($("select#reaction").val());
-    var size = $("select#size").val();
-    var mobile = $("input:radio[name=mobile]:checked").val();
+    var size = parseInt($("select#size").val());
+    var mobile = parseInt($("input:radio[name=mobile]:checked").val());
     var system = $("input:radio[name=os]:checked").val();
-    var freeTime = $("select#time").val();
+    var freeTime = parseInt($("select#time").val());
 
-    if (website === 0) {
+    if (website + freeTime <= 0) {
     $('#trackid').text("CSS/Design");
-    } else if (website === 1) {
+    } else if (website > 0 & mobile >= 1) {
     $('#trackid').text('Java/Android');
-    } else if (website > 1) {
+    } else if (website > 0 & size >= 1 & mobile <1) {
     $('#trackid').text('C#/.Net');
     }
 
